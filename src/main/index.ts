e2a581eb6faf3668609d1e2ec9f25c2b5f0bf591
Piet Ostendorp <79719028+Laubfrosch49@users.noreply.github.com>
2026-01-24 19:40:24 +0100

    Start Application with max size and fix auto reload

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,20 +1,28 @@
 import { join } from 'node:path'
+import { is } from '@electron-toolkit/utils'
 import { app, BrowserWindow } from 'electron'
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    show: false,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: join(__dirname, '../preload/index.ts'),
+      sandbox: false,
     },
   })
 
-  if (process.env['ELECTRON_RENDERER_URL']) {
+  mainWindow.maximize()
+
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
 }
 
 app.whenReady().then(createWindow)
